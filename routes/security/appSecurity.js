8bc9192cfee591f5c574bccebfba0cb3a2ccb40d
Andres Perez <Your Actual Email>
2018-10-09 18:52:13 -0500

    Science and Language description

--- routes/security/appSecurity.js
+++ routes/security/appSecurity.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const SECURITY = {
-  appVersion: "0.01c",
+  appVersion: "0.02",
   secret: process.env.SECURITY_SECRET,
   token(docs) {
     return new Promise((resolve, reject) => {
