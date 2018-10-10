81b40e38e734a66b33790d789efa8e117aaefb67
Andres Perez <Your Actual Email>
2018-10-10 11:55:29 -0500

    Scroll Behavior Vue

--- src/router.js
+++ src/router.js
@@ -113,5 +113,12 @@ export default new Router({
       name: "reset",
       component: ResetPassword
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
