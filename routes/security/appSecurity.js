edb2c3759f1c154856ea0b264f89a76d441e8d5b
Andres Perez <Your Actual Email>
2018-10-16 10:06:57 -0500

    App version 0.07

--- routes/security/appSecurity.js
+++ routes/security/appSecurity.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const SECURITY = {
-  appVersion: "0.06",
+  appVersion: "0.07",
   secret: process.env.SECURITY_SECRET,
   token(docs) {
     return new Promise((resolve, reject) => {
